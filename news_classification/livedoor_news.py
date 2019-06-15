95d1f7f0f632489816f7e3745978a3f2e78643a6
Ida Kenichiro <ida.kenichiro@gmail.com>
2019-06-16 00:52:48 +0900

    Install keras to use tokenizer_from_json()

--- news_classification/livedoor_news.py
+++ news_classification/livedoor_news.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-import pickle
 
 import numpy as np
+import keras
+from keras_preprocessing.text import Tokenizer, tokenizer_from_json
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
 
 from .japanese_tokenizer import MeCabTokenizer
 
@@ -57,7 +57,7 @@ def load_directory_data(directory):
 
 
 def save_data(num_words=None):
-    tar_path = tf.keras.utils.get_file(
+    tar_path = keras.utils.get_file(
         "ldcc-20140209.tar.gz",
         "https://www.rondhuit.com/download/ldcc-20140209.tar.gz",
         cache_subdir="datasets/livedoor_news",
@@ -73,7 +73,7 @@ def save_data(num_words=None):
         texts += site_texts
         labels += [label] * len(site_texts)
 
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=num_words)
+    tokenizer = Tokenizer(num_words=num_words)
     tokenizer.fit_on_texts(texts)
 
     with livedoor_news.joinpath("livedoor_news.npz").open("wb") as npz:
@@ -81,8 +81,8 @@ def save_data(num_words=None):
             npz, x=tokenizer.texts_to_matrix(texts, mode="tfidf"), y=np.array(labels)
         )
 
-    with livedoor_news.joinpath("livedoor_news_tokenizer.pickle").open("wb") as pkl:
-        pickle.dump(tokenizer, pkl)
+    with livedoor_news.joinpath("livedoor_news_tokenizer.json").open("w") as json_file:
+        json_file.write(tokenizer.to_json())
 
 
 def load_data(num_words=None, test_split=0.2):
@@ -103,11 +103,11 @@ def load_data(num_words=None, test_split=0.2):
 
 def get_tokenizer():
     path = Path(
-        "~/.keras/datasets/livedoor_news/livedoor_news_tokenizer.pickle"
+        "~/.keras/datasets/livedoor_news/livedoor_news_tokenizer.json"
     ).expanduser()
 
     if not path.exists():
-        raise ValueError("save_data() must be called before taking tokenizer.")
+        raise RuntimeError("load_data() must be invoked before taking tokenizer.")
 
-    with path.open("rb") as pkl:
-        return pickle.load(pkl)
+    with path.open("r") as json_file:
+        return tokenizer_from_json(json_file.read())
--- news_classification/model.py
+++ news_classification/model.py
@@ -1,6 +1,4 @@
-from pathlib import Path
-
-import tensorflow as tf
+import keras
 
 from .livedoor_news import load_data
 
@@ -8,29 +6,21 @@ from .livedoor_news import load_data
 def create_model(path="news_classification_model.h5"):
     (x_train, y_train), (x_test, y_test) = load_data()
 
-    y_train = tf.keras.utils.to_categorical(y_train)
-    y_test = tf.keras.utils.to_categorical(y_test)
+    y_train = keras.utils.to_categorical(y_train)
+    y_test = keras.utils.to_categorical(y_test)
 
     input_shape = (x_train.shape[1],)
     units = y_train.shape[1]
 
-    model = tf.keras.Sequential(
+    model = keras.Sequential(
         [
-            tf.keras.layers.Dense(64, activation="relu", input_shape=input_shape),
-            tf.keras.layers.Dropout(0.5),
-            tf.keras.layers.Dense(64, activation="relu"),
-            tf.keras.layers.Dropout(0.5),
-            tf.keras.layers.Dense(units, activation="softmax"),
+            keras.layers.Dense(64, activation="relu", input_shape=input_shape),
+            keras.layers.Dropout(0.5),
+            keras.layers.Dense(64, activation="relu"),
+            keras.layers.Dropout(0.5),
+            keras.layers.Dense(units, activation="softmax"),
         ]
     )
-    # model = tf.keras.Sequential([
-    #     tf.keras.layers.Embedding(2000, 128, input_length=x_train.shape[1]),
-    #     tf.keras.layers.Conv1D(32, 7, activation="relu"),
-    #     tf.keras.layers.MaxPooling1D(5),
-    #     tf.keras.layers.Conv1D(32, 7, activation="relu"),
-    #     tf.keras.layers.GlobalMaxPooling1D(),
-    #     tf.keras.layers.Dense(units, activation="softmax"),
-    # ])
     model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["acc"])
     model.summary()
     model.fit(x_train, y_train, epochs=5, batch_size=16, validation_split=0.2)
@@ -44,4 +34,4 @@ def create_model(path="news_classification_model.h5"):
 
 
 def load_model(path="news_classification_model.h5"):
-    return tf.keras.models.load_model(path)
+    return keras.models.load_model(path)
