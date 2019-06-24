e50326a2963228846632e0eb9955c9a2e479edc0
Ida Kenichiro <ida.kenichiro@gmail.com>
2019-06-25 01:02:02 +0900

    Measure metrics for all labels

--- news_classifier/model.py
+++ news_classifier/model.py
@@ -1,7 +1,19 @@
 import keras
 import keras_metrics as km
+import pandas as pd
 
-from .livedoor_news import load_data
+from .livedoor_news import load_data, get_classifications
+
+
+def metrics():
+    functions = ["acc"]
+
+    for label, _ in enumerate(get_classifications()):
+        functions.append(km.categorical_precision(label=label))
+        functions.append(km.categorical_recall(label=label))
+        functions.append(km.categorical_f1_score(label=label))
+
+    return functions
 
 
 def create_model(path="news_classifier_model.h5"):
@@ -23,14 +35,7 @@ def create_model(path="news_classifier_model.h5"):
         ]
     )
     model.compile(
-        optimizer="rmsprop",
-        loss="categorical_crossentropy",
-        metrics=[
-            "acc",
-            km.categorical_precision(),
-            km.categorical_recall(),
-            km.categorical_f1_score(),
-        ],
+        optimizer="rmsprop", loss="categorical_crossentropy", metrics=metrics()
     )
     model.summary()
     model.fit(x_train, y_train, epochs=5, batch_size=16, validation_split=0.2)
@@ -38,9 +43,13 @@ def create_model(path="news_classifier_model.h5"):
     test_metrics = model.evaluate(x_test, y_test)
     print(f"test_loss: {test_metrics[0]:.4f}")
     print(f"test_acc: {test_metrics[1]:.4f}")
-    print(f"test_precision: {test_metrics[2]:.4f}")
-    print(f"test_recall: {test_metrics[3]:.4f}")
-    print(f"test_f1_score: {test_metrics[4]:.4f}")
+
+    df = pd.DataFrame(
+        data=[test_metrics[i : i + 3] for i in range(2, len(test_metrics), 3)],
+        index=[description for _, description in get_classifications()],
+        columns=["test_precision", "test_recall", "test_f1_score"],
+    )
+    print(df)
 
     model.save(path)
 
