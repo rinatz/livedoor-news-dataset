f2acd6bf04063c39da41740bdf2a311d50837c7f
Ida Kenichiro <ida.kenichiro@gmail.com>
2020-04-25 17:03:38 +0900

    Add docopt

--- Makefile
+++ Makefile
@@ -2,10 +2,14 @@
 init:
 	poetry install --no-root
 
+.PHONY: get
+get:
+	poetry run python -m news_classifier get
+
 .PHONY: fit
 fit:
-	poetry run python -m news_classifier
+	poetry run python -m news_classifier fit
 
 .PHONY: run
 run:
-	streamlit run main.py
+	poetry run streamlit run main.py
--- news_classifier/__main__.py
+++ news_classifier/__main__.py
@@ -1,10 +1,27 @@
+"""Create a model for news classification.
+
+Usage:
+    news_classifier get
+    news_classifier fit
+
+Arguments:
+    get     Get dataset
+    fit     Fit a model
+"""
+
+from docopt import docopt
+
 from .livedoor_news import save_data
 from .training import fit_model
 
 
-def main():
-    save_data()
-    fit_model()
+def main(argv=None):
+    args = docopt(__doc__, argv=argv)
+
+    if args["get"]:
+        save_data()
+    elif args["fit"]:
+        fit_model()
 
 
 if __name__ == "__main__":
